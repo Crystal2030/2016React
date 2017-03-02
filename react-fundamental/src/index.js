74f7255aabd5fb7d4ee6c05da4294cc2fd296d11
liuyun <liuyun@ihangmei.com>
2017-03-02 17:29:29 +0800

    form operation

--- react-fundamental/src/index.js
+++ react-fundamental/src/index.js
@@ -2,7 +2,7 @@
 import React, {Component} from 'React';
 import ReactDOM, {render} from 'react-dom';
 
-import UseChildrenComponent from "./MyComponent/ListComponent"
+import FormApp from "./MyComponent/Form/FormApp"
 
 //props数据
 const obj = {
@@ -13,5 +13,5 @@ const obj = {
 //嵌入变量全部放入花括号里{}
 //如果obj有几十个属性，可以通过 {...obj} 来批量设置一个对象的键值对到组件的属性
 //name = {obj.name} type={obj.type}
-render(<UseChildrenComponent />, document.getElementById('app'));
+render(<FormApp />, document.getElementById('app'));
 
