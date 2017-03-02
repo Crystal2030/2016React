cef5bff87a0feedd29edff08742f2027a7c36001
liuyun <liuyun@ihangmei.com>
2017-03-02 16:38:04 +0800

    LifeCycle

--- react-fundamental/src/index.js
+++ react-fundamental/src/index.js
@@ -2,7 +2,7 @@
 import React, {Component} from 'React';
 import ReactDOM, {render} from 'react-dom';
 
-import StateDemo from "./MyComponent"
+import Destroy from "./LifeCycle/Destroy"
 
 //props数据
 const obj = {
@@ -13,7 +13,5 @@ const obj = {
 //嵌入变量全部放入花括号里{}
 //如果obj有几十个属性，可以通过 {...obj} 来批量设置一个对象的键值对到组件的属性
 //name = {obj.name} type={obj.type}
-render(<StateDemo status={true}
-    {...obj}
-/>, document.getElementById('app'));
+render(<Destroy />, document.getElementById('app'));
 
