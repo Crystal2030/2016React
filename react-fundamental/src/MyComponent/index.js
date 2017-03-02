c135366f78685d6ce05bee834cf33411595afd58
liuyun <liuyun@ihangmei.com>
2017-03-02 16:15:32 +0800

    props and state

--- react-fundamental/src/MyComponent/index.js
+++ react-fundamental/src/MyComponent/index.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react';
+//组件 其实就是 状态机
+// props（输入）-》组件内部逻辑(state)->html
 
-class SpreadDemo extends Component {
-    componentWillMount(){
-        console.log(this.props);
+import React, { Component, PropTypes} from 'react';
+
+/*const defaultProps = {
+    status: 'crysatl'
+}*/
+class StateDemo extends Component {
+
+    constructor(){
+        super();//继承
+        this.state = {
+            secondsElapsed: 0
+        }
     }
+
+    /*state = {
+        secondsElapsed: 0
+    }*/
+
+    tick(){
+        this.setState({ secondsElapsed: this.state.secondsElapsed + 1 });
+    }
+
+    componentDidMount(){
+        this.interval = setInterval( this.tick.bind(this), 1000 );
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval);
+    }
+
     render(){
-        return <h1> {this.props.name} is a {this.props.type} </h1>;
+        {/*初始渲染state*/}
+        console.log(this);
+        return (
+            <div>目前已经计时：{this.state.secondsElapsed}秒, 初始的：{this.props.status}</div>
+        )
     }
 }
 
-export default SpreadDemo;
+StateDemo.defaultProps = {
+    status: 'sssssss'
+};
+StateDemo.propTypes = {
+    status: React.PropTypes.string
+};
+export default StateDemo;
--- react-fundamental/src/index.js
+++ react-fundamental/src/index.js
@@ -2,16 +2,18 @@
 import React, {Component} from 'React';
 import ReactDOM, {render} from 'react-dom';
 
-import SpreadDemo from "./MyComponent"
+import StateDemo from "./MyComponent"
 
+//props数据
 const obj = {
     name: 'crystal',
-    type: 'female'
+    type: 'female',
+    aa: ["red", "blue", "yellow"]
 };
 //嵌入变量全部放入花括号里{}
 //如果obj有几十个属性，可以通过 {...obj} 来批量设置一个对象的键值对到组件的属性
 //name = {obj.name} type={obj.type}
-render(<SpreadDemo
+render(<StateDemo status={true}
     {...obj}
 />, document.getElementById('app'));
 
