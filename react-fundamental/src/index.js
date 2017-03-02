ed7d614cb4ecd7bffbf4010df17c6c4f24123b22
liuyun <liuyun@ihangmei.com>
2017-03-02 17:16:00 +0800

    component nest

--- react-fundamental/src/index.js
+++ react-fundamental/src/index.js
@@ -2,7 +2,7 @@
 import React, {Component} from 'React';
 import ReactDOM, {render} from 'react-dom';
 
-import HandleEvent from "./MyComponent/HandleEvent"
+import UseChildrenComponent from "./MyComponent/ListComponent"
 
 //props数据
 const obj = {
@@ -13,5 +13,5 @@ const obj = {
 //嵌入变量全部放入花括号里{}
 //如果obj有几十个属性，可以通过 {...obj} 来批量设置一个对象的键值对到组件的属性
 //name = {obj.name} type={obj.type}
-render(<HandleEvent />, document.getElementById('app'));
+render(<UseChildrenComponent />, document.getElementById('app'));
 
