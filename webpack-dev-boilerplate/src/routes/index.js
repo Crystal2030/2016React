d47b00d98bb6fafe791da186f7ea0dadf84e17c6
liuyun <liuyun@ihangmei.com>
2017-03-03 19:04:56 +0800

    Add Link

--- webpack-dev-boilerplate/src/routes/index.js
+++ webpack-dev-boilerplate/src/routes/index.js
@@ -1,9 +1,35 @@
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory , Link} from 'react-router';
 import { App } from '../containers';
 import React from 'react';
 
+const Home = () =>
+    <div>
+        <Links />
+        <h1>Home</h1>
+    </div>
+const About = () =>
+    <div>
+        <Links />
+        <h1>About</h1>
+    </div>
+const Contact = () =>
+    <div>
+        <Links />
+        <h1>Contact</h1>
+    </div>
 export default (
   <Router history={browserHistory}>
-    <Route path="/" component={App} />
+      <Route path="/" component={App} />
+      <Route path="/home" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
   </Router>
 )
+
+
+const Links = () =>
+    <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+    </nav>
