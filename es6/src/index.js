d110503b43e6f73dc3e6d7b113bed83186e4e6d0
liuyun <liuyun@ihangmei.com>
2017-03-06 11:49:40 +0800

    es6: arrow funciton

--- es6/src/index.js
+++ es6/src/index.js
@@ -16,3 +16,22 @@ var fn4 = (a, b) => {
 }
 
 console.log(fn4(1, 2));//6
+
+var obj = {
+    name: 'crystal',
+    courses: ["react", "vue.js", "node.js"],
+    //es5
+    getMessage: function () {
+        this.courses.forEach(function(item){
+            console.log(this.name + " tech us " + item);
+        }.bind(this))
+    },
+    //es6
+    getMessage2: function(){
+        this.courses.forEach((item) => {
+            console.log(this.name + " tech us " + item);
+        })
+    }
+}
+
+obj.getMessage2();
