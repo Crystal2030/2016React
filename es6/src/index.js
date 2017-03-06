c5e8de7632e4214ac224abf39e771ea109dfaa6a
liuyun <liuyun@ihangmei.com>
2017-03-06 11:59:58 +0800

    es6: 类，继承

--- es6/src/index.js
+++ es6/src/index.js
@@ -1,37 +1,48 @@
-var fn = p => p;
-
-var fn1 = () => "无参数输入的箭头函数";
-
-var fn2 = (a,b) => a + b;
-
-var fn3 = ({a,b}) => a + b;
-
-console.log(fn3({a:1,b:3}));//4
-
-var fn4 = (a, b) => {
-    var m = a + 2;
-
-    //显式return
-    return m*b;
+/*
+//es5 方式定义类
+function Animal(name, age){
+    this.name = name;
+    this.age = age;
 }
 
-console.log(fn4(1, 2));//6
-
-var obj = {
-    name: 'crystal',
-    courses: ["react", "vue.js", "node.js"],
-    //es5
-    getMessage: function () {
-        this.courses.forEach(function(item){
-            console.log(this.name + " tech us " + item);
-        }.bind(this))
-    },
-    //es6
-    getMessage2: function(){
-        this.courses.forEach((item) => {
-            console.log(this.name + " tech us " + item);
-        })
+Animal.prototype = {
+    getMessage: function(){
+        console.log(this.name + " is " + this.age + ' years old');
     }
 }
 
-obj.getMessage2();
+
+var cat = new Animal('cat', 1);
+
+cat.getMessage();*/
+
+// es6方式定义类
+class Animal {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    getMessage(){
+        console.log(this.name + " is " + this.age + ' years old');
+    }
+
+    static showInfo(){
+        console.log('show info');
+    }
+}
+//继承
+class Cat extends Animal{
+    constructor(name, age, color){
+        super(name, age);
+        this.color = color;
+    }
+    getName(){
+        console.log(this.name + "'s color is "+this.color)
+    }
+}
+var cat = new Cat('cat', 1, 'grey');
+console.log(cat.__proto__)
+
+cat.getName();
+
+Animal.showInfo();
