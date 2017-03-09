33215734cd978d64e42cf0155264cc4af35c5905
liuyun <liuyun@ihangmei.com>
2017-03-09 11:08:45 +0800

    嵌套route， indexRoute，路由变量

--- webpack-dev-boilerplate/src/containers/App/index.jsx
+++ webpack-dev-boilerplate/src/containers/App/index.jsx
@@ -1,27 +1,67 @@
 import React, { Component } from 'react';
-import { Router, Route, browserHistory, Link } from 'react-router';
+import { Router, Route, browserHistory, Link, IndexRoute } from 'react-router';
+import "./index.css";
 
-const Home = () => <div><Links/><h1>Home</h1></div>;
+/*const Home = () => <div><Links/><h1>Home</h1></div>;
 const About = () => <div><Links/><h1>About</h1></div>;
-const Contact = () => <div><Links/><h1>Contact</h1></div>;
+const Contact = () => <div><Links/><h1>Contact</h1></div>;*/
+
+/*const Home = (props) =>
+    <div>
+        <Links />
+        <h1>Home</h1>
+        {props.children}
+    </div>
+
+const About = (props) =>
+    <div>
+        <h1>我是默认展示的路由</h1>
+        {props.children}
+    </div>
+
+const Contact = () =>
+    <div>
+        <h1>Contact</h1>
+    </div>*/
+
+//路由变量
+class Message extends Component {
+    render(){
+        console.log(this.props);
+        return (
+            <div>
+                <h1>{this.props.params.message || 'Home'}</h1>
+                <Links />
+            </div>
+        )
+    }
+}
 
 class App extends Component {
     render() {
         return (
             <Router history={browserHistory}>
-                <Route path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/contact" component={Contact} />
+                <Route path="/(:message)" component={Message} />
+                {/* <Route path="/" component={Home}>
+                    <IndexRoute component={About} />
+                    <Route path="about" component={About}>
+                        <Route path="contact" component={Contact} />
+                    </Route>
+                </Route>*/}
             </Router>
         );
     }
 }
-
+/**
+ * '/'   对应的是 Home组件
+ * '/about' 对应的是 Home + About
+ * '/about/contact' 对应的是 Home + About + Contact
+ */
 const Links = () =>
     <nav>
-        <Link to="/">Home  </Link>
-        <Link to="/about">About  </Link>
-        <Link to="/contact">Contact</Link>
+        <Link activeClassName="active" to="/">Home  </Link>
+        <Link activeClassName="active" to="/about">About  </Link>
+        <Link activeClassName="active" to="/contact">Contact</Link>
     </nav>
 
 export default App;
