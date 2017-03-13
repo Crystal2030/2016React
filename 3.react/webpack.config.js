05df8b11e0627080420f0b93b1c8bdd5c09ba4d3
liuyun <liuyun@ihangmei.com>
2017-03-13 17:19:18 +0800

    webpack config modified

--- 3.react/webpack.config.js
+++ 3.react/webpack.config.js
@@ -5,50 +5,57 @@ var uglifyPlugin = webpack.optimize.UglifyJsPlugin;//plugin
 
 console.log(env);
 
-var libraryName = 'sparrow';
-var plugins = [];
-var filename = '';
-if(env === 'production'){
-    plugins.push(new uglifyPlugin({minimize: true}));
-    //生产环境：sparrow.min.js
-    filename = libraryName + '.min.js';
-    console.log(filename)
-}else{
-    //开发环境：sparrow.js
-    filename = libraryName + '.js';
+
+
+
+
+// module.exports = config;
+
+module.export = function(env){
+    var libraryName = 'sparrow';
+    var plugins = [];
+    var filename = '';
+    if(env === 'production'){
+        plugins.push(new uglifyPlugin({minimize: true}));
+        //生产环境：sparrow.min.js
+        filename = libraryName + '.min.js';
+        console.log(filename)
+    }else{
+        //开发环境：sparrow.js
+        filename = libraryName + '.js';
+    }
+
+    var config = {
+        entry: path.resolve(__dirname, './src/index.js'),
+        output: {
+            path: path.resolve(__dirname, './lib'),
+            filename: filename,
+            library: libraryName,
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        },
+        devtool: "cheap-source-map"
+        ,
+        resolve: {
+            extensions: ['.js', '.css', 'json']
+        },
+        module:{
+            loaders: [
+                {
+                    test: /\.js&/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.js&/,
+                    loader: 'eslint',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        plugins: plugins
+
+    };
+
+    return config;
 }
-
-
-var config = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    output: {
-        path: path.resolve(__dirname, './lib'),
-        filename: filename,
-        library: libraryName,
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    devtool: "cheap-source-map"
-    ,
-    resolve: {
-        extensions: ['.js', '.css', 'json']
-    },
-    module:{
-        loaders: [
-            {
-                test: /\.js&/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.js&/,
-                loader: 'eslint',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    plugins: plugins
-
-};
-
-module.exports = config;
