d6ee5b0ad66c123f5596ac4d028f5c1eb28a49cb
liuyun <liuyun@ihangmei.com>
2017-03-15 15:22:50 +0800

    modified List component

--- homework3/src/component/List/index.js
+++ homework3/src/component/List/index.js
@@ -1,52 +1,18 @@
 import React, {Component, Children} from 'react';
 import ReactDOM, {render} from 'react-dom';
 import "./index.css";
+
 export default class List extends Component {
-    constructor () {
-        super();
-        this.state = {
-            data: [
-                {
-                    id: 1,
-                    content: 'zhufeng',
-                    bgColor: 'red'
-                },
-                {
-                    id: 2,
-                    content: 'react',
-                    bgColor: "blue"
-                },
-                {
-                    id: 3,
-                    content: 'component',
-                    bgColor: 'green'
-                }
-            ]
-        };
-    }
-    save = (e) => {
-
-    }
-    edit = (e) => {
-
-    }
     render() {
-        console.log(this.state.data)
-
+        console.log(this.props.data)
+        let items = this.props.data.map(function(item){
+            return (
+                <li key={item.id} className={item.bgColor}>{item.content}</li>
+            )
+        })
         return (
             <ul>
-                <li className={this.state.data[0].bgColor}>
-                    {this.state.data[0].content}
-                    <button onClick={this.edit}>编辑</button>
-                </li>
-                <li className={this.state.data[1].bgColor}>
-                    {this.state.data[1].content}
-                    <button onClick={this.edit}>编辑</button>
-                </li>
-                <li className={this.state.data[2].bgColor}>
-                    {this.state.data[2].content}
-                    <button onClick={this.edit}>编辑</button>
-                </li>
+                {items}
             </ul>
         )
     }
--- homework3/src/index.js
+++ homework3/src/index.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 import ReactDOM, {render} from 'react-dom';
 import List from './component/List';
-
+var data = [
+    {
+        id: 1,
+        content: 'zhufeng',
+        bgColor: 'red'
+    },
+    {
+        id: 2,
+        content: 'react',
+        bgColor: "blue"
+    },
+    {
+        id: 3,
+        content: 'component',
+        bgColor: 'green'
+    }
+];
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            data: data
+        }
+    }
     render(){
         return (
-            <List/>
+            <List data ={this.state.data} />
+
         )
     }
 }
