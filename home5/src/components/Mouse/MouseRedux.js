f2e28121b1125b8d0b3a832e3ea0188de439eb92
crystal2030 <867389458@qq.com>
2017-04-02 00:29:56 +0800

    modify mouse game4

--- home5/src/components/Mouse/MouseRedux.js
+++ home5/src/components/Mouse/MouseRedux.js
@@ -1,5 +1,5 @@
 let timer;
-
+let getRound
 export function Hit(id){
     return (dispatch, getState) => {
         dispatch({type: "HIT", id: id});
@@ -7,26 +7,45 @@ export function Hit(id){
 }
 export function Start(){
 	return (dispatch, getState) => {
-		let getRound = setTimeout(() => {
-			let data = getState();
-			let hasUnhitMouse = data.items.find(item => item.img === 'mouse');
-			let hasHitMouse = data.items.find(item => item.img === 'mouse_cry');
-			console.log(!hasUnhitMouse , !!hasHitMouse, '7777777777');
-			if(hasUnhitMouse){
-				clearInterval(timer);
-				clearTimeout(getRound);
-				alert('Game Over');
-				return;
-			}
+		let round = () => {
+			getRound = setTimeout(function(){
+				let data = getState();
+				let hasUnhitMouse = data.items.find(item => item.img === 'mouse');
+				let hasHitMouse = data.items.find(item => item.img === 'mouse_cry');
+				console.log(hasUnhitMouse , hasHitMouse, '77777');
+				// 如果所有地鼠都被击打过，则进入下一回合；
+				if(!hasUnhitMouse && !!hasHitMouse){
+					console.log('11111111111111111111')
+					clearTimeout(getRound);
+					alert('Win, all mouses are hit! Next round!');
+					round();
+					return;
+				}else if(!!hasUnhitMouse && !hasUnhitMouse){
+					console.log('2222222222222222222')
+					// 如果有没有打击过的地鼠，则Game Over
+					clearInterval(timer);
+					clearTimeout(getRound);
+					alert('Game Over');
+					return;
+				}
 
-			if(!hasUnhitMouse && !!hasHitMouse){
-				clearInterval(timer);
-				clearTimeout(getRound);
-				alert('Win, all mouses are hit!');
-				return;
-			}
+				if(data.count > 5){
+					console.log('3333333333333333333333')
+					clearInterval(timer);
+					clearTimeout(getRound);
+					alert('Round ' + data.count + ', pass!');
+					return;
+				}
 
-		}, 5000)
+				data.count++;
+
+				console.log('4444444444444444444444444',data.count)
+			}, 2000)
+		}
+		if(!getRound){
+			console.log(!getRound, '!!!!!!!!!!')
+			round();
+		}
 		timer = setInterval(() => {
 			return dispatch({type: "BEGIN"});
 		}, 1000)
