5a7cee9376fec079340791c91070020c13c40f82
crystal2030 <867389458@qq.com>
2017-04-10 23:15:26 +0800

    react-resume basic architecture

---
+++ 7.react-resume/src/main.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import './main.css';
+
+import App from './layouts/App';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
