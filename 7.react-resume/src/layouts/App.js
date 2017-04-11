80cd1d8c420c91b88fbd94414ad0d397ab73eb7d
crystal2030 <867389458@qq.com>
2017-04-11 22:30:12 +0800

    react-resume 添加react-addons-css-transition-group实现动画切换

--- 7.react-resume/src/layouts/App.js
+++ 7.react-resume/src/layouts/App.js
@@ -1,11 +1,25 @@
 import React, {Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+import './App.css';
 
 class App extends Component{
 	render(){
 		return (
 			<div>
 				<h1>这是我的在线个人网站!</h1>
-				<div>{this.props.children}</div>
+				<div className="main">
+					<ReactCSSTransitionGroup
+						component="div"
+						className="app"
+					    transitionName="app"
+					    transitionEnterTimeout={1000}
+					    transitionLeaveTimeout={1000}>
+						<div className="content" key={this.props.location.pathname}>
+							{this.props.children}
+						</div>
+					</ReactCSSTransitionGroup>
+				</div>
 			</div>
 		)
 	}
