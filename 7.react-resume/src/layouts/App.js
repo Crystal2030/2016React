26b7b17a94a2316e35dccb0c49f2979cf69b2b36
crystal2030 <867389458@qq.com>
2017-04-11 22:08:06 +0800

    react-resume 添加container下个组件，搭建react router

--- 7.react-resume/src/layouts/App.js
+++ 7.react-resume/src/layouts/App.js
@@ -1,5 +1,14 @@
 import React, {Component } from 'react';
 
-const App = (props) => <div>App component </div>
+class App extends Component{
+	render(){
+		return (
+			<div>
+				<h1>这是我的在线个人网站!</h1>
+				<div>{this.props.children}</div>
+			</div>
+		)
+	}
+}
 
 export default App;
